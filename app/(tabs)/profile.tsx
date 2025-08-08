6a0b09e52ae4cf1ca6aad535b10b7c50eddbd27a
Andy C <caozsn@gmail.com>
2025-08-08 22:52:01 +1000

    sum up

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -3,8 +3,8 @@ import { Text, View } from "react-native";
 
 export default function Profile() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-5xl font-bold text-primary">Profile</Text>
+    <View className="flex-1 items-center justify-center bg-primary">
+      <Text className="text-5xl font-bold text-light-100">Profile</Text>
     </View>
   );
 }
--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -3,8 +3,8 @@ import { Text, View } from "react-native";
 
 export default function Saved() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-5xl font-bold text-primary">Saved Movies</Text>
+    <View className="flex-1 items-center justify-center bg-primary">
+      <Text className="text-5xl font-bold text-light-100">Saved Movies</Text>
     </View>
   );
 }
